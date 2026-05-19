4468c85231157baf3ddde46f55cfba647600c054
Yashaswini Kothapalli <yashaswinikothapalli@gmail.com>
2026-05-19 13:09:09 +0530

    Updated css

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "main" / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
